4f528e0214fd57b0ebeaffb393c0ad3464901a35
@Flaeng <flaeng@users.noreply.github.com>
2023-07-14 20:05:09 +0200

    Updated actions

--- build/Build.GitHubActions.cs
+++ build/Build.GitHubActions.cs
@@ -2,14 +2,15 @@
     "PR",
     GitHubActionsImage.UbuntuLatest,
     InvokedTargets = new[] { nameof(Format), nameof(Test), nameof(Stryker) },
-    OnPullRequestBranches = new[] { "main" }
+    OnPullRequestBranches = new[] { "main" },
+    PublishArtifacts = false
 )]
 [GitHubActions(
-    "Deploy",
+    "Build",
     GitHubActionsImage.UbuntuLatest,
-    OnPushTags = new[] { @"v*.*.*" },
-    InvokedTargets = new[] { nameof(Publish) },
-    PublishArtifacts = true
+    InvokedTargets = new[] { nameof(Stryker) },
+    OnPushBranches = new [] { "main" },
+    PublishArtifacts = false
 )]
 partial class Build
 {
