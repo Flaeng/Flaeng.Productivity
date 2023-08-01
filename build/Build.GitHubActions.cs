59c83c29593b87052428d5bf0f6de30bab500a6e
@Flaeng <flaeng@users.noreply.github.com>
2023-08-01 20:08:49 +0200

    Added build to sln and added path filter to push on main

--- build/Build.GitHubActions.cs
+++ build/Build.GitHubActions.cs
@@ -10,6 +10,7 @@
     GitHubActionsImage.WindowsLatest,
     InvokedTargets = new[] { nameof(Stryker) },
     OnPushBranches = new[] { "main" },
+    OnPushIncludePaths = new [] { "src/**" },
     PublishArtifacts = true
 )]
 partial class Build
